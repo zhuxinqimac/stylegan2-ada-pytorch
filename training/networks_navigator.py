6cdc1c294c7319d4c513728647dc352a28e00473
zhuxinqimac <zhuxinqimac@yahoo.com>
2021-05-19 02:39:42 +1000

    lerp loss

--- training/networks_navigator.py
+++ training/networks_navigator.py
@@ -8,7 +8,7 @@
 
 # --- File Name: networks_navigator.py
 # --- Creation Date: 27-04-2021
-# --- Last Modified: Wed 19 May 2021 01:47:50 AEST
+# --- Last Modified: Wed 19 May 2021 02:39:38 AEST
 # --- Author: Xinqi Zhu
 # .<.<.<.<.<.<.<.<.<.<.<.<.<.<.<.<
 """
@@ -240,13 +240,13 @@ class Navigator(torch.nn.Module):
             x = torch.ones(x_in.size(0), self.z_dim, 1).to(x_in.device) # (1, z_dim, 1)
             layer = getattr(self, f'fc0')
             x = layer(x)
-        # x = normalize_2nd_moment_to_one(x, dim=-1) * 0.02
-        # x = normalize_2nd_moment_to_one(x, dim=-1) * self.sample_var_scale(x)
+        # x = normalize_2nd_moment(x, dim=-1) * 0.02
+        # x = normalize_2nd_moment(x, dim=-1) * self.sample_var_scale(x)
         # print('x.len:', torch.norm(x, dim=-1).squeeze())
         if self.use_local_layer_heat:
-            dir_x = normalize_2nd_moment_to_one(x[:, :, :self.w_dim], dim=-1)
+            dir_x = normalize_2nd_moment(x[:, :, :self.w_dim], dim=-1)
             heat_x = x[:, :, self.w_dim:]
             x = torch.cat([dir_x, heat_x], dim=-1)
         else:
-            x = normalize_2nd_moment_to_one(x, dim=-1)
+            x = normalize_2nd_moment(x, dim=-1)
         return x
