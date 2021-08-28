d6a9e0441f43a6955fb7121c163dcd42af0c5896
zhuxinqimac <zhuxinqimac@yahoo.com>
2021-08-28 14:04:33 +1000

    make I_loss and I_g_loss equal

--- train_group.py
+++ train_group.py
@@ -8,7 +8,7 @@
 
 # --- File Name: train_group.py
 # --- Creation Date: 22-08-2021
-# --- Last Modified: Sat 28 Aug 2021 14:03:01 AEST
+# --- Last Modified: Sat 28 Aug 2021 14:03:47 AEST
 # --- Author: Xinqi Zhu
 # .<.<.<.<.<.<.<.<.<.<.<.<.<.<.<.<
 """
@@ -170,9 +170,9 @@ def setup_training_loop_kwargs(
         'celeba-experiment-I1': dict(ref_gpus=2, kimg=25000,  mb=32, mbstd=4, fmaps=0.125, lrate=0.002, gamma=10, ema=10,  ramp=0.05, n_samples_per=7,
                              z_dim=64, use_noise=True, lie_alg_init_scale=0.001, group_mat_dim=20, proj_feat_size=64, proj_feat_ch=8, projector_type='flatten',
                              post_exp_conv_feat_base=8, commute_lamb=0, hessian_lamb=0.01, I_lambda=1, I_g_lambda=0),
-        'celeba-hessian-I-hpc': dict(ref_gpus=2, kimg=25000,  mb=32, mbstd=4, fmaps=0.125, lrate=0.002, gamma=10, ema=10,  ramp=0.05, n_samples_per=7,
+        'celeba-hessian-I1-hpc': dict(ref_gpus=2, kimg=25000,  mb=32, mbstd=4, fmaps=0.125, lrate=0.002, gamma=10, ema=10,  ramp=0.05, n_samples_per=7,
                              z_dim=64, use_noise=True, lie_alg_init_scale=0.001, group_mat_dim=20, proj_feat_size=128, proj_feat_ch=4, projector_type='flatten',
-                             post_exp_conv_feat_base=4, commute_lamb=0, hessian_lamb=1, I_lambda=0, I_g_lambda=1.),
+                             post_exp_conv_feat_base=4, commute_lamb=0, hessian_lamb=1, I_lambda=1, I_g_lambda=0.),
         'celeba-hessian-z32-I1-hpc': dict(ref_gpus=2, kimg=25000,  mb=32, mbstd=4, fmaps=0.125, lrate=0.002, gamma=10, ema=10,  ramp=0.05, n_samples_per=7,
                              z_dim=32, use_noise=True, lie_alg_init_scale=0.001, group_mat_dim=20, proj_feat_size=128, proj_feat_ch=4, projector_type='flatten',
                              post_exp_conv_feat_base=4, commute_lamb=0, hessian_lamb=1, I_lambda=1, I_g_lambda=0),
