d5124559e9946aa1e56451acd8042fa821318d64
zhuxinqimac <zhuxinqimac@yahoo.com>
2021-10-01 02:19:21 +1000

    discover add use_pca_sign

--- training/loss_discover.py
+++ training/loss_discover.py
@@ -8,7 +8,7 @@
 
 # --- File Name: loss_discover.py
 # --- Creation Date: 27-04-2021
-# --- Last Modified: Fri 01 Oct 2021 01:55:11 AEST
+# --- Last Modified: Fri 01 Oct 2021 02:19:16 AEST
 # --- Author: Xinqi Zhu
 # .<.<.<.<.<.<.<.<.<.<.<.<.<.<.<.<
 """
@@ -578,6 +578,7 @@ class DiscoverLoss(Loss):
                 # Sample variation scales.
                 if self.use_dynamic_scale:
                     if self.use_pca_sign:
+                        print('using pca_sign ')
                         scale_pos = ((torch.randn(b//2, device=delta.device).abs() * self.var_sample_scale * step_scale_pos + self.var_sample_mean) * step_sign_pos).view(b//2, 1, 1)
                         scale_neg = ((torch.randn(b//2, device=delta.device).abs() * self.var_sample_scale * step_scale_neg + self.var_sample_mean) * step_sign_neg).view(b//2, 1, 1)
                     else:
